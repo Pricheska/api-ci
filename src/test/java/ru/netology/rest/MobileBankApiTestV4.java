9d8b893513bfe7432fbd535f6fe4c4a144ecdcf8
pricheska <den-ryab2019@yandex.ru>
2023-03-21 02:24:53 +0400

    testing CI

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -3,7 +3,9 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV4 {
     @Test
@@ -18,7 +20,7 @@ class MobileBankApiTestV4 {
       // Проверки
       .then()
           .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-      ;
+             .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
     }
 }
